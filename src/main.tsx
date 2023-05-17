56dfb7337d5a6a4ee2d8d0c134efe6c4db7ca7ab
soniafarhat123 <sonia.farhat@esprit.tn>
2023-05-17 17:33:00 +0200

    pwa test

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { Storage, Drivers } from "@ionic/storage";
 
-const container = document.getElementById('root');
+const store = new Storage({ name: "__mydb", driverOrder: [Drivers.IndexedDB] });
+await store.create();
+await store.set("name", { id_document: "3123AZEA", name: "ajdajhjk" });
+const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import legacy from "@vitejs/plugin-legacy";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
+  plugins: [react(), VitePWA({ registerType: "autoUpdate" }), legacy()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
-})
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+  },
+});
